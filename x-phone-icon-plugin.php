838fe5125de512c452ce8b0e7edd7bf7ff9d49b8
Alex Moise <alex@moise.pro>
2018-08-14 11:51:40 +0300

    Phone number changed

--- x-phone-icon-plugin.php
+++ x-phone-icon-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: X Theme Phone Icon
  * Plugin URI: https://github.com/alexmoise/X-Theme-Mobile-Phone-Icon
  * Description: A simple plugin to add a callable phone icon to the left side of X theme bar on mobiles. No settings page at this moment (maybe make the phone number and icon color as manageable options in the future?). For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -12,7 +12,7 @@
 add_action( 'x_before_view_global__brand', 'mo_add_my_icon', 10 );
 function mo_add_my_icon() {
  // if ( current_user_can(update_core) ) { // uncomment for playing with it as admin only, also see below
-  echo "<a href=\"tel:1300794892\" class=\"x-phone-icon\"> <img src=\"". plugin_dir_url( __FILE__ ) . 'x-phone-icon.png'."\"> </a>";
+  echo "<a href=\"tel:1300208580\" class=\"x-phone-icon\"> <img src=\"". plugin_dir_url( __FILE__ ) . 'x-phone-icon.png'."\"> </a>";
  // } // uncomment for playing with it as admin only, also see above
 }
 
